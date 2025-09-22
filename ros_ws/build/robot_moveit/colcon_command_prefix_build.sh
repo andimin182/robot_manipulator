2c4f20a1249039a0182d4c391bf9d1ca9bcb729b
andimin182 <andimindru@gmail.com>
2025-09-22 14:01:02 +0200

    clean

--- ros_ws/build/robot_moveit/colcon_command_prefix_build.sh
+++
@@ -1,4 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/root/ros_ws/install/moveit_task_constructor_msgs/share/moveit_task_constructor_msgs/package.sh"
-. "/root/ros_ws/install/rviz_marker_tools/share/rviz_marker_tools/package.sh"
-. "/root/ros_ws/install/moveit_task_constructor_core/share/moveit_task_constructor_core/package.sh"
